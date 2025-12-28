46a33776020e3c9157b1c3524543507253a53a39
DDDonaldWu <donaldwuhz@gmail.com>
2025-12-28 21:33:50 +0900

    Create i18n.js

---
+++ i18n.js
@@ -0,0 +1,14 @@
+const I18N = {
+  zh: {
+    hero: {
+      title: '北海道二世古私人滑雪課程',
+      subtitle: '適合初學者到進階的客製化滑雪教學'
+    }
+  },
+  en: {
+    hero: {
+      title: 'Private Ski/Snowboard Lessons in Niseko',
+      subtitle: 'Personalized lessons for beginners to advanced skiers'
+    }
+  }
+};
